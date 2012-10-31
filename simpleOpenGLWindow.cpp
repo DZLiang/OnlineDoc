fdeb71f8d081b165f206f4dbd576f7864ce56d9d
DZLiang <357478351@qq.com>
2012-10-31 23:21:18 +0800

    this is a test
    first test

---
+++ simpleOpenGLWindow.cpp
@@ -0,0 +1,11 @@
+#include <GLUT/GLUT.h>
+#include <OpenGL/OpenGL.h>
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+      printf("%s", "output something");
+      return 0;
+      glutInit(&argc, argv);
+      return 0;
+      return 0;
+}
